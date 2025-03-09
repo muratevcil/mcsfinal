76c759006a9403d7404a3df8e7047101ecdbff63
muratevcil <muratevcilf@gmail.com>
2025-03-09 18:42:52 +0300

    initial commit

---
+++ src/main/java/mcs/mcsfinal2100005222/Mcsfinal2100005222Application.java
@@ -0,0 +1,13 @@
+package mcs.mcsfinal2100005222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcsfinal2100005222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mcsfinal2100005222Application.class, args);
+    }
+
+}
---
+++ src/test/java/mcs/mcsfinal2100005222/Mcsfinal2100005222ApplicationTests.java
@@ -0,0 +1,13 @@
+package mcs.mcsfinal2100005222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcsfinal2100005222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
